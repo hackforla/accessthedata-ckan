26d0041354526dddb3e4b15e53782084f6362f7b
Tony Delgado-Willis <tony.delgado.willis@gmail.com>
2023-05-24 17:46:44 -0700

    Clean up comments from CSS pipeline

--- ckan/docker-entrypoint.d/build-css.sh
+++ ckan/docker-entrypoint.d/build-css.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-echo "build-css is hapenning"
-echo $(pwd)
-
 npx postcss \
     /srv/app/src_extensions/ckanext-hack4laatd/ckanext/hack4laatd/assets/src/css/theme.css \
-    -o /srv/app/src_extensions/ckanext-hack4laatd/ckanext/hack4laatd/assets/theme2.css \
-    --verbose
-
-
+    -o /srv/app/src_extensions/ckanext-hack4laatd/ckanext/hack4laatd/assets/theme.css
